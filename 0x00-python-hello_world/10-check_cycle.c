af7da692bf00baf982e6c331e8ef36e701905f0c
odudu <iamgpower3@gmail.com>
2022-05-31 05:58:37 +0100

    first commit

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+* check_cycle - function
+* @list: list to check
+*
+* Description: function to check list if there is a cycle
+* Return: 0 if no cycle, 1 if cycle
+*/
+int check_cycle(listint_t *list)
+{
+	if (list == 0)
+		return (0);
+	else
+		return (1);
+}
---
+++ 0x00-python-hello_world/README.md
@@ -0,0 +1,2 @@
+# 0x00 Python- Hello, World
+[ALX School Python Programming](https://github.com/iamGpower/alx-higher_level_programming)
